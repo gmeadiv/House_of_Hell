2f4823580021290b18405d874ad43921445c1e67
Anthony Morton <anthonymorton760@gmail.com>
2021-08-31 21:08:14 -0700

    created about me page and added everyone's profile pictures.

---
+++ js/aboutUs.js
@@ -0,0 +1,28 @@
+function popUp1() {
+  var popup = document.getElementById('anthonyPopUp');
+  popup.classList.toggle('show');
+  console.log('this is working')
+}
+function popUp2() {
+  var popup = document.getElementById('andreaPopUp');
+  popup.classList.toggle('show');
+  console.log('this is working')
+}
+function popUp3() {
+  var popup = document.getElementById('georgePopUp');
+  popup.classList.toggle('show');
+  console.log('this is working')
+}
+function popUp4() {
+  var popup = document.getElementById('zackPopUp');
+  popup.classList.toggle('show');
+  console.log('this is working')
+}
+function popUp5() {
+  var popup = document.getElementById('roopPopUp');
+  popup.classList.toggle('show');
+  console.log('this is working')
+}
+
+
+console.log('hello world')
